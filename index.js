c3c2db929220e16d6223339484b29f604c92dad9
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-10-28 17:28:46 -0400

    met all specs

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById('time').value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+const greet = timeString => {
+  if (parseInt(timeString) < 12) {
+    return 'Good Morning';
+  } else if (parseInt(timeString) <= 17) {
+    return 'Good Afternoon';
+  }
+  return 'Good Evening';
+};
+
 /* Write your implementation of displayMessage() */
+const displayMessage = message => {
+  const greeting = document.getElementById('greeting');
+  greeting.innerText = message;
+};
