ee8d5c5c86038cf181bb5d18a5e461ca4a227601
Danny Lee <dreamer@panix.com>
2019-10-29 21:49:39 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  var hours = time.split(":");
+
+  if (hours[0] < 12) {
+    return "Good Morning"
+  }
+  else if (hours[0] > 12 && hours[0] < 17) {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(str) {
+  document.getElementById("greeting").innerText = str;
+}
